2146590ed109cb14236a42671462e5821bff969f
Oguz <oguz2863@gmail.com>
2024-04-08 17:39:17 +0200

    Init

---
+++ lib/questions_summary.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class QuestionsSummary extends StatelessWidget {
+  const QuestionsSummary(this.summaryData, {super.key});
+
+  final List<Map<String, Object>> summaryData;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 300,
+      child: SingleChildScrollView(
+        child: Column(
+          children: summaryData.map(
+            (data) {
+              return Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(0, 0, 30, 0),
+                    child: Text(
+                      ((data['question_index'] as int) + 1).toString(),
+                      style: GoogleFonts.jetBrainsMono(
+                        fontSize: 20,
+                        color: data['user_answer'] == data['correct_answer']
+                            ? Colors.lightGreen
+                            : Colors.red,
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          data['question'] as String,
+                          style: GoogleFonts.jetBrainsMono(
+                            color: Colors.white,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 5,
+                        ),
+                        Text(
+                          data['user_answer'] as String,
+                          style: GoogleFonts.jetBrainsMono(
+                            color: Colors.grey,
+                          ),
+                        ),
+                        Text(
+                          data['correct_answer'] as String,
+                          style: GoogleFonts.jetBrainsMono(
+                            color: Colors.greenAccent,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 30,
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              );
+            },
+          ).toList(),
+        ),
+      ),
+    );
+  }
+}
